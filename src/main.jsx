e5c0676f5edad7db414d4dc3776a6afc098638cc
anthomars <anthomars.am@gmail.com>
2026-09-22 06:43:45 +0700

    fix path

--- src/main.jsx
+++ src/main.jsx
@@ -1,9 +1,9 @@
-import { StrictMode } from 'react'
+﻿import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import App from './App.jsx'
 import { BrowserRouter } from 'react-router-dom'
 import 'bootstrap/dist/css/bootstrap.min.css'
-import './dist/css/main.css'
+import './css/main.css'
 import ScrollToTop from './components/ScrollToTop.jsx'
 import 'animate.css'
 import AOS from 'aos';
